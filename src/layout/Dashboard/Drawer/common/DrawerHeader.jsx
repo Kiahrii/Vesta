a458cda7ac800c48b003025d0d8c610d5ef294c8
Keziah Garcia <keziahgarciagotoc25@gmail.com>
2026-03-25 20:52:00 +0800

    add logo

--- src/layout/Dashboard/Drawer/common/DrawerHeader.jsx
+++ src/layout/Dashboard/Drawer/common/DrawerHeader.jsx
@@ -7,14 +7,12 @@ import Image from 'react-bootstrap/Image';
 import { APP_DEFAULT_PATH } from 'config';
 
 // assets
-import logo from 'assets/images/logo-white.svg';
+import logo from 'assets/images/lowgow.svg';
 
 export const DrawerHeader = () => {
   return (
     <div className="m-header">
-      <Link to={APP_DEFAULT_PATH} className="b-brand text-primary">
-        <Image src={logo} fluid className="logo logo-lg" alt="logo" />
-      </Link>
+      <Image src={logo} fluid className="logo logo-lg" alt="logo" />
     </div>
   );
 };
